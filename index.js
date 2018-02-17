b59b1ce16739a87021ae0a97962700007f59c616
Olle Wahlström <olle.wahlstrom@a3j.se>
2018-02-17 17:19:37 +0100

    awd

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+'use strict';
+
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.use(express.static('./public'))
+//app.use(express.static(__dirname + '/public'))
+
+app.use('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'))
+})
 
 app.listen(3000, () => {
   console.log("listening on 3000")
