85dbc6bcbde9f4c1588517e17268763128746a9b
Olle Wahlström <olle.wahlstrom@a3j.se>
2018-02-20 20:03:34 +0100

    lol

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-//app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '/public', 'index.html'))
